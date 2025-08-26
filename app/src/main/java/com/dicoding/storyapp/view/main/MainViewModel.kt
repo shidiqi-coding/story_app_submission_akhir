12d60cdad3d6ea794cd71d3843f35bfa655b7695
shidiqi-coding <shidiqiihsan@gmail.com>
2025-08-26 10:21:04 +0700

    update Unit test ke package test

--- app/src/main/java/com/dicoding/storyapp/view/main/MainViewModel.kt
+++ app/src/main/java/com/dicoding/storyapp/view/main/MainViewModel.kt
@@ -8,19 +8,21 @@ import androidx.paging.cachedIn
 import com.dicoding.storyapp.StoryRepository
 import com.dicoding.storyapp.data.ResultState
 import com.dicoding.storyapp.data.database.StoryEntity
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 
 @Suppress("UNCHECKED_CAST")
-class MainViewModel(private val repository: StoryRepository) : ViewModel() {
+class MainViewModel(
+    private val repository: StoryRepository,
+    coroutineScope: CoroutineScope? = null
+) : ViewModel() {
 
+    private val scope = coroutineScope ?: viewModelScope
 
     val stories: Flow<PagingData<StoryEntity>> =
-        repository.getStories().cachedIn(viewModelScope)
+        repository.getStories().cachedIn(scope)
 
     fun getNewStory(): LiveData<ResultState<List<StoryEntity>>> {
         return repository.getNewStory()
     }
-
-
-
 }
