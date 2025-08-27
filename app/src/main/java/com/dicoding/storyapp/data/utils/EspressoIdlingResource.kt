3e6416bb7f8207421df6f48a64fc213d16847a5e
Shidiqi Coding <shidiqiihsan@gmail.com>
2025-08-27 20:45:09 +0700

    Update EspressoIdlingResource.kt

--- app/src/main/java/com/dicoding/storyapp/data/utils/EspressoIdlingResource.kt
+++ app/src/main/java/com/dicoding/storyapp/data/utils/EspressoIdlingResource.kt
@@ -1,19 +1,20 @@
-
 package com.dicoding.storyapp.utils
 
 import androidx.test.espresso.idling.CountingIdlingResource
 
 object EspressoIdlingResource {
     private const val RESOURCE = "GLOBAL"
-    @JvmField val idlingResource = CountingIdlingResource(RESOURCE)
+
+    @JvmField
+    val countingIdlingResource = CountingIdlingResource(RESOURCE)
 
     fun increment() {
-        idlingResource.increment()
+        countingIdlingResource.increment()
     }
 
     fun decrement() {
-        if (!idlingResource.isIdleNow) {
-            idlingResource.decrement()
+        if (!countingIdlingResource.isIdleNow) {
+            countingIdlingResource.decrement()
         }
     }
 }
