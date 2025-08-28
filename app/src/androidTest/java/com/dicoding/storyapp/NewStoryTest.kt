875eb93fc2625abb43bc233df73df91600de46bd
Shidiqi Coding <shidiqiihsan@gmail.com>
2025-08-28 21:29:05 +0700

    Update NewStoryTest.kt

--- app/src/androidTest/java/com/dicoding/storyapp/NewStoryTest.kt
+++ app/src/androidTest/java/com/dicoding/storyapp/NewStoryTest.kt
@@ -1,95 +1,106 @@
-package com.dicoding.storyapp
+ackage com.dicoding.storyapp
 
-import android.app.Instrumentation
-import android.content.Intent
-import android.net.Uri
-import android.provider.MediaStore
-import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingPolicies
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.*
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.GrantPermissionRule
 import com.dicoding.storyapp.utils.EspressoIdlingResource
-import com.dicoding.storyapp.view.main.MainActivity
-import com.dicoding.storyapp.view.newstory.NewStoryActivity
-import org.hamcrest.Matchers.allOf
+import com.dicoding.storyapp.view.authenticator.login.LoginActivity
 import org.junit.*
 import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
 class NewStoryTest {
 
     @get:Rule
-    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
-        android.Manifest.permission.READ_EXTERNAL_STORAGE,
-        android.Manifest.permission.CAMERA
-    )
+    val activityRule = ActivityScenarioRule(LoginActivity::class.java)
 
     @Before
     fun setUp() {
+        // Atur timeout IdlingResource biar ga cepat gagal
+        IdlingPolicies.setIdlingResourceTimeout(60, TimeUnit.SECONDS)
+        IdlingPolicies.setMasterPolicyTimeout(60, TimeUnit.SECONDS)
+
+        // Register IdlingResource
         IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
-        Intents.init()
     }
 
     @After
     fun tearDown() {
+        // Unregister setelah test selesai
         IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
-        Intents.release()
     }
 
+    /**
+     * Fungsi helper untuk login dan dismiss dialog sukses login
+     */
+    private fun loginToApp() {
+        onView(withId(R.id.usernameInput))
+            .perform(typeText("test@email.com"), closeSoftKeyboard())
+        onView(withId(R.id.passwordInput))
+            .perform(typeText("12345678"), closeSoftKeyboard())
+        onView(withId(R.id.loginButton)).perform(click())
+
+        // ✅ Handle dialog sukses login
+        onView(withText("congratulations!"))
+            .check(matches(isDisplayed()))
+        onView(withText("continue"))
+            .perform(click())
+
+        // Pastikan RecyclerView tampil setelah login
+        onView(withId(R.id.rvStoryList))
+            .check(matches(isDisplayed()))
+    }
+
+    /**
+     * Test: mencoba menambahkan cerita tanpa memilih gambar
+     */
     @Test
-    fun addStoryFlow() {
+    fun testAddStoryWithoutImage() {
+        loginToApp()
 
-        ActivityScenario.launch(MainActivity::class.java)
-
-
+        // Klik tombol tambah cerita (FAB)
         onView(withId(R.id.fabAddStory)).perform(click())
 
-
-        onView(withId(R.id.descriptionEditText)).check(matches(isDisplayed()))
-
-
+        // Isi deskripsi tanpa gambar
         onView(withId(R.id.descriptionEditText))
-            .perform(typeText("Cerita dari UI test"), closeSoftKeyboard())
-
-
-        val expectedIntent = allOf(
-            hasAction(Intent.ACTION_PICK),
-            hasData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        )
-
-        val resultData = Intent().apply {
-            data = Uri.parse("android.resource://${ApplicationProvider.getApplicationContext<android.content.Context>().packageName}/drawable/ic_launcher_background")
-        }
-        val result = Instrumentation.ActivityResult(android.app.Activity.RESULT_OK, resultData)
-        Intents.intending(expectedIntent).respondWith(result)
-
-        onView(withId(R.id.galleryButton)).perform(click())
-
-      
+            .perform(typeText("Testing story"), closeSoftKeyboard())
         onView(withId(R.id.uploadButton)).perform(click())
 
-  
-        onView(withId(R.id.rvStoryList)).check(matches(isDisplayed()))
+        // Cek apakah muncul pesan error
+        onView(withText("Please select an image first"))
+            .check(matches(isDisplayed()))
     }
 
+    /**
+     * Test: menambahkan cerita dengan gambar berhasil
+     */
     @Test
-    fun testCancelAddStory() {
-        ActivityScenario.launch(MainActivity::class.java)
+    fun testAddStoryWithImage() {
+        loginToApp()
 
+        // Klik tombol tambah cerita (FAB)
         onView(withId(R.id.fabAddStory)).perform(click())
 
- 
-        onView(withContentDescription("Navigate up")).perform(click())
+        // Pilih gambar dari galeri
+        onView(withId(R.id.galleryButton)).perform(click())
+        // ⬆️ Biasanya kamu perlu tambahkan Intent Stub / Espresso-Intents
+        // untuk mock pilih gambar. Kalau sudah di-mock, ini cukup.
 
-        onView(withId(R.id.rvStoryList)).check(matches(isDisplayed()))
+        // Isi deskripsi
+        onView(withId(R.id.descriptionEditText))
+            .perform(typeText("Story with image"), closeSoftKeyboard())
+
+        // Upload
+        onView(withId(R.id.uploadButton)).perform(click())
+
+        // Cek apakah muncul notifikasi / dialog sukses
+        onView(withText("Story uploaded successfully"))
+            .check(matches(isDisplayed()))
     }
-
 }
